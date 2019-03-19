bb8734906aee44c7b455f7e1c45a7b2e6f20584b
andychan516 <623247195@qq.com>
2019-03-19 20:33:26 +0800

    hello 123

--- TestDemo/src/com/andy/demo/Test.java
+++ TestDemo/src/com/andy/demo/Test.java
@@ -2,7 +2,7 @@ package com.andy.demo;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello 123");
 		int a=3;
 		System.out.println(a);
 	}

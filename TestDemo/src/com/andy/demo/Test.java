c744022bc853f7844e562e4626c0c718b7b6e968
andychan516 <623247195@qq.com>
2019-04-16 20:51:53 +0800

    修改

--- TestDemo/src/com/andy/demo/Test.java
+++ TestDemo/src/com/andy/demo/Test.java
@@ -3,7 +3,7 @@ package com.andy.demo;
 public class Test {
 	public static void main(String[] args) {
  
-		System.out.println("hello 123");
+		System.out.println("hello andy");
 		int a=3;
 		System.out.println(a);
  
